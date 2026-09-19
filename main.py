b83a00d5526afc7ce7e80ea52c557284c5f3baaf
GuilhermeGaliazzo <guilhermemassari.rodrigues@gmail.com>
2026-09-18 22:57:36 -0300

    Adiciona a procura por livro específico utilizando o índice e o tratamento do erro 404.

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
+clients = [
+   "Generic Name 1",
+   "Generic Name 2",
+   "Generic Name 3",
+   "Generic Name 4"
+]
+
 @app.get("/")
-async def root():
-    return {"Oi"}
+async def home():
+   return {"Welcome to my client management system."}
+
+@app.get("/all-clients")
+async def all_clients():
+   return {"All clients" : clients}
+
+@app.get("/search-client/{number}")
+async def search_number(number: int):
+    if number < 0 or number > len(clients):
+       raise HTTPException(404, "Index out of range")
+    else:
+       return {"client" : clients[number]}
